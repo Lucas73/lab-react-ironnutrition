9a9cf4148d7cdb99b74b08f45927fa8c6faf87b9
Lucas <lucas@MacBook-Air-de-Lucas.local>
2019-02-25 21:23:53 +0100

    fit Header & ProductBox

---
+++ starter-code/src/misc/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+const Header = () => (
+  <section className="hero is-dark">
+  <div className="hero-body">
+    <div className="container">
+      <h1 className="title">
+        IronNutrition
+      </h1>
+    </div>
+  </div>
+</section>
+);
+
+export default Header;
